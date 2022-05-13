c1c8a492885f0f0f4724a29269252f6a8b7d1e9e
Jan-Jaap Spies <jan-jaap.spies@hva.nl>
2022-05-13 22:04:38 +0200

    Initial commit bubbleBank application

---
+++ src/main/java/nl/hva/miw/c27/team1/cryptobanking/BubbleBankApplication.java
@@ -0,0 +1,13 @@
+package nl.hva.miw.c27.team1.cryptobanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbleBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BubbleBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/hva/miw/c27/team1/cryptobanking/BubbleBankApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hva.miw.c27.team1.cryptobanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubbleBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
