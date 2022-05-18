ca36e6105f4e2ba7e6f29a6e1355b138978adc1a
David Kwaadgras <david.kwaadgras@hva.nl>
2022-05-18 14:19:10 +0200

    fixed conflict hopefully

--- src/main/java/nl/hva/miw/c27/team1/cryptobanking/repository/dao/JdbcProfileDao.java
+++ src/main/java/nl/hva/miw/c27/team1/cryptobanking/repository/dao/JdbcProfileDao.java
@@ -2,10 +2,16 @@ package nl.hva.miw.c27.team1.cryptobanking.repository.dao;
 
 import nl.hva.miw.c27.team1.cryptobanking.model.Profile;
 import nl.hva.miw.c27.team1.cryptobanking.model.Transaction;
+import nl.hva.miw.c27.team1.cryptobanking.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -23,6 +29,26 @@ public class JdbcProfileDao implements ProfileDao {
 
     @Override
     public Optional<Profile> findByUserName(String userName) {
-        return Optional.empty();
+        List<Profile> profiles =
+                jdbcTemplate.query(("select * from profile where username = ?"), new ProfileRowMapper(), userName);
+        if (profiles.size() != 1) {
+            return Optional.empty();
+        } else {
+            return Optional.of(profiles.get(0));
+        }
+
     }
+
+    private static class ProfileRowMapper implements RowMapper<Profile> {
+
+        @Override
+        public Profile mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            String userName = resultSet.getString("userName");
+            String password = resultSet.getString("password");
+            int userId = resultSet.getInt("userId");
+            return new Profile(userName, password, new User(userId));
+
+        }
+    }
+
 }
--- src/main/java/nl/hva/miw/c27/team1/cryptobanking/repository/dao/JdbcUserDao.java
+++ src/main/java/nl/hva/miw/c27/team1/cryptobanking/repository/dao/JdbcUserDao.java
@@ -4,8 +4,15 @@ import nl.hva.miw.c27.team1.cryptobanking.model.Transaction;
 import nl.hva.miw.c27.team1.cryptobanking.model.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.*;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,19 +21,78 @@ public class JdbcUserDao implements UserDao {
     private JdbcTemplate jdbcTemplate;
     private final Logger logger = LogManager.getLogger(Transaction.class);
 
-
+    @Autowired
     public JdbcUserDao(JdbcTemplate jdbcTemplate) {
         super();
         this.jdbcTemplate = jdbcTemplate;
         logger.info("New JdbcUserDao.");
     }
 
-    @Override
-    public void save(User user) {
+    private PreparedStatement insertUserStatement(User user, Connection connection) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(
+                "insert into user (firstname, prefix, surname, dateofbirth, fiscalnumber, streetname," +
+                        "housenumber, zipcode, residence, country, role, staffId) values (?, ?, ?, ?, ?, ?, ?," +
+                        "?, ?, ?, ?, ?)",
+                Statement.RETURN_GENERATED_KEYS);
+        ps.setString(1, user.getFirstName());
+        ps.setString(2, user.getPrefix());
+        ps.setString(3, user.getSurName());
+        ps.setDate(4, (Date) user.getBirthDate());
+        ps.setInt(5, user.getBsnNumber());
+        ps.setString(6, user.getStreetName());
+        ps.setString(7, user.getHouseNumber());
+        ps.setString(8, user.getZipCode());
+        ps.setString(9, user.getResidence());
+        ps.setString(10, user.getCountry());
+        ps.setString(11, user.getRole());
+        ps.setInt(12, user.getStaffId);
+
+        return ps;
     }
 
     @Override
+    public void save(User user) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> insertUserStatement(user, connection), keyHolder);
+        int newKey = keyHolder.getKey().intValue();
+        user.setId(newKey);
+    }
+
+
+    @Override
     public Optional<User> findById(int id) {
-        return Optional.empty();
+        List<User> users =
+                jdbcTemplate.query("select * from user where userId = ?", new UserRowMapper(), id);
+        if (users.size() != 1) {
+            return Optional.empty();
+        } else {
+            return Optional.of(users.get(0));
+        }
+    }
+
+
+    private static class UserRowMapper implements RowMapper<User> {
+
+        @Override
+        public User mapRow(ResultSet resultSet, int rowNum) throws SQLException {
+            int id = resultSet.getInt("userid");
+            String firstname = resultSet.getString("full_name");
+            String prefix = resultSet.getString("username");
+            String surname = resultSet.getString("password");
+            Date date = resultSet.getDate("dateOfBirth");
+            int fiscalnumber = resultSet.getInt("fiscalnumber");
+            String streetname = resultSet.getString("streetname");
+            String housenumber = resultSet.getString("housenumber");
+            String zipcode = resultSet.getString("zipcode");
+            String residence = resultSet.getString("residence");
+            String country = resultSet.getString("country");
+            String role = resultSet.getString("role");
+            int staffId = resultSet.getInt(("staffId");
+
+            User user = new User(id, firstname, prefix, surname, date, fiscalnumber, streetname, housenumber, zipcode,
+                    residence, country, role, staffId);
+            user.setId(id);
+            return user;
+        }
     }
 }
